66fb7bd3ca759ca5c2e12184c7677234c2bd8467
lvaldenebro <lauravaldeal@gmail.com>
2022-11-03 20:26:29 +0100

    Rewrite code for toggle management in forms and optimizes it

--- src/js/5_create-card.js
+++ src/js/5_create-card.js
@@ -12,56 +12,99 @@ const fillArrowUp = document.querySelector('.js-fill-arrow-up');
 const fillArrowDown = document.querySelector('.js-fill-arrow-down');
 const shareArrowUp = document.querySelector('.js-share-arrow-up');
 const shareArrowDown = document.querySelector('.js-share-arrow-down');
+const allHead = [designHead, fillHead, shareHead]; //array
 
-
-
-
-designHead.addEventListener('click', (event) => {
-    event.preventDefault();
-    //console.log('Hola');
-    if (designBigBox.classList.contains('collapsed')){
-        designBigBox.classList.remove('collapsed');
-        designArrowDown.classList.remove('collapsed');
-        designArrowUp.classList.add('collapsed');
-    }
-    else{
-        //console.log('macarena');
-        designBigBox.classList.add('collapsed');
-        designArrowDown.classList.add('collapsed');
-        designArrowUp.classList.remove('collapsed');
-    }
-  }); 
-
-
-  fillHead.addEventListener('click', (event) => {
-    event.preventDefault();
-    //console.log('Hola');
-    if (fillBigBox.classList.contains('collapsed')){
+function reviewClickPlace(actualClickPlace) {
+    switch (actualClickPlace) {
+    case fillHead:
         fillBigBox.classList.remove('collapsed');
-        fillArrowDown.classList.remove('collapsed');
         fillArrowUp.classList.add('collapsed');
-    }
-    else{
-        //console.log('macarena');
-        fillBigBox.classList.add('collapsed');
-        fillArrowDown.classList.add('collapsed');
-        fillArrowUp.classList.remove('collapsed');
-    }
-  }); 
-
-  shareHead.addEventListener('click', (event) => {
-    event.preventDefault();
-    //console.log('Hola');
-    if (shareBigBox.classList.contains('collapsed')){
+        fillArrowDown.classList.remove('collapsed');
+        break;
+    case shareHead:
         shareBigBox.classList.remove('collapsed');
-        shareArrowDown.classList.remove('collapsed');
         shareArrowUp.classList.add('collapsed');
+        shareArrowDown.classList.remove('collapsed');
+        break;
+    case designHead: //to avoid possible error if something changes
+        designBigBox.classList.remove('collapsed');
+        designArrowUp.classList.add('collapsed');
+        designArrowDown.classList.remove('collapsed');
     }
-    else{
-        //console.log('macarena');
-        shareBigBox.classList.add('collapsed');
-        shareArrowDown.classList.add('collapsed');
-        shareArrowUp.classList.remove('collapsed');
-    }
-  }); 
+}
+
+function addClass() {
+    //Boxes
+    designBigBox.classList.add('collapsed');
+    fillBigBox.classList.add('collapsed');
+    shareBigBox.classList.add('collapsed');
+    // arrows
+    designArrowUp.classList.remove('collapsed');
+    designArrowDown.classList.add('collapsed');
+    fillArrowUp.classList.remove('collapsed');
+    fillArrowDown.classList.add('collapsed');
+    shareArrowUp.classList.remove('collapsed');
+    shareArrowDown.classList.add('collapsed');
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    addClass();
+    reviewClickPlace(event.currentTarget); //because we have here the listener
+}
+
+for (let selector of allHead) {
+    selector.addEventListener('click', handleClick);
+}
+
+
+
+// designHead.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     //console.log('Hola');
+//     if (designBigBox.classList.contains('collapsed')){
+//         designBigBox.classList.remove('collapsed');
+//         designArrowDown.classList.remove('collapsed');
+//         designArrowUp.classList.add('collapsed');
+//     }
+//     else{
+//         //console.log('macarena');
+//         designBigBox.classList.add('collapsed');
+//         designArrowDown.classList.add('collapsed');
+//         designArrowUp.classList.remove('collapsed');
+//     }
+//   }); 
+
+
+//   fillHead.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     //console.log('Hola');
+//     if (fillBigBox.classList.contains('collapsed')){
+//         fillBigBox.classList.remove('collapsed');
+//         fillArrowDown.classList.remove('collapsed');
+//         fillArrowUp.classList.add('collapsed');
+//     }
+//     else{
+//         //console.log('macarena');
+//         fillBigBox.classList.add('collapsed');
+//         fillArrowDown.classList.add('collapsed');
+//         fillArrowUp.classList.remove('collapsed');
+//     }
+//   }); 
+
+//   shareHead.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     //console.log('Hola');
+//     if (shareBigBox.classList.contains('collapsed')){
+//         shareBigBox.classList.remove('collapsed');
+//         shareArrowDown.classList.remove('collapsed');
+//         shareArrowUp.classList.add('collapsed');
+//     }
+//     else{
+//         //console.log('macarena');
+//         shareBigBox.classList.add('collapsed');
+//         shareArrowDown.classList.add('collapsed');
+//         shareArrowUp.classList.remove('collapsed');
+//     }
+//   }); 
 
