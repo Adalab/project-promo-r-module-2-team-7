33cf386c95deba6faf3e48d96fa9c432a1397ed2
Marta Castrillo <mart.castrillo@gmail.com>
2022-11-03 14:16:03 +0100

    try new js fucntions

--- src/js/5_create-card.js
+++ src/js/5_create-card.js
@@ -14,54 +14,108 @@ const shareArrowUp = document.querySelector('.js-share-arrow-up');
 const shareArrowDown = document.querySelector('.js-share-arrow-down');
 
 
-
-
-designHead.addEventListener('click', (event) => {
+function handleClickCollapsablesDesign (event){
     event.preventDefault();
-    //console.log('Hola');
     if (designBigBox.classList.contains('collapsed')){
+        console.log('AQUI');
         designBigBox.classList.remove('collapsed');
         designArrowDown.classList.remove('collapsed');
         designArrowUp.classList.add('collapsed');
-    }
-    else{
-        //console.log('macarena');
-        designBigBox.classList.add('collapsed');
-        designArrowDown.classList.add('collapsed');
-        designArrowUp.classList.remove('collapsed');
-    }
-  }); 
-
-
-  fillHead.addEventListener('click', (event) => {
-    event.preventDefault();
-    //console.log('Hola');
-    if (fillBigBox.classList.contains('collapsed')){
-        fillBigBox.classList.remove('collapsed');
-        fillArrowDown.classList.remove('collapsed');
-        fillArrowUp.classList.add('collapsed');
-    }
-    else{
-        //console.log('macarena');
         fillBigBox.classList.add('collapsed');
         fillArrowDown.classList.add('collapsed');
         fillArrowUp.classList.remove('collapsed');
-    }
-  }); 
-
-  shareHead.addEventListener('click', (event) => {
-    event.preventDefault();
-    //console.log('Hola');
-    if (shareBigBox.classList.contains('collapsed')){
-        shareBigBox.classList.remove('collapsed');
-        shareArrowDown.classList.remove('collapsed');
-        shareArrowUp.classList.add('collapsed');
-    }
-    else{
-        //console.log('macarena');
         shareBigBox.classList.add('collapsed');
         shareArrowDown.classList.add('collapsed');
         shareArrowUp.classList.remove('collapsed');
     }
-  }); 
+       /* i'm not sure if this else if does anything, i try to force it to have at least open design */
+    /* else if((fillBigBox.classList.contains('collapsed') )&& (shareBigBox.classList.contains('collapsed'))){
+        console.log('AQUI NO');
+
+        shareBigBox.classList.add('collapsed');
+        shareArrowDown.classList.remove('collapsed');
+        shareArrowUp.classList.add('collapsed');
+        fillBigBox.classList.add('collapsed');
+        fillArrowDown.classList.add('collapsed');
+        fillArrowUp.classList.remove('collapsed');
+    } */
+    else{
+        ('AQUI SI');
+        designBigBox.classList.add('collapsed');
+        designArrowDown.classList.add('collapsed');
+        designArrowUp.classList.remove('collapsed');
+    }
+  }; 
+
+  function handleClickCollapsablesFill (event){
+    event.preventDefault();
+    if (fillBigBox.classList.contains('collapsed')){
+        fillBigBox.classList.remove('collapsed');
+        fillArrowDown.classList.remove('collapsed');
+        fillArrowUp.classList.add('collapsed');
+        designBigBox.classList.add('collapsed');
+        designArrowDown.classList.add('collapsed');
+        designArrowUp.classList.remove('collapsed');
+        shareBigBox.classList.add('collapsed');
+        shareArrowDown.classList.add('collapsed');
+        shareArrowUp.classList.remove('collapsed');
+    }
+       /* i'm not sure if this else if does anything, i try to force it to have at least open design */
+    else if((fillBigBox.classList.contains('collapsed') )&& (shareBigBox.classList.contains('collapsed'))){
+        designBigBox.classList.remove('collapsed');
+        designArrowDown.classList.remove('collapsed');
+        designArrowUp.classList.add('collapsed');
+        shareBigBox.classList.add('collapsed');
+        shareArrowDown.classList.remove('collapsed');
+        shareArrowUp.classList.add('collapsed');
+        fillBigBox.classList.add('collapsed');
+        fillArrowDown.classList.add('collapsed');
+        fillArrowUp.classList.remove('collapsed');
+    }
+    else{
+     
+        fillBigBox.classList.add('collapsed');
+        fillArrowDown.classList.add('collapsed');
+        fillArrowUp.classList.remove('collapsed');
+    }
+  }; 
+  
+  function handleClickCollapsablesShare (event){
+    event.preventDefault();
+    if (shareBigBox.classList.contains('collapsed')){
+        shareBigBox.classList.remove('collapsed');
+        shareArrowDown.classList.remove('collapsed');
+        shareArrowUp.classList.add('collapsed');
+        fillBigBox.classList.add('collapsed');
+        fillArrowDown.classList.add('collapsed');
+        fillArrowUp.classList.remove('collapsed');
+         designBigBox.classList.add('collapsed');
+         designArrowDown.classList.add('collapsed');
+         designArrowUp.classList.remove('collapsed');
+    }
+     /* i'm not sure if this else if does anything, i try to force it to have at least open design */
+    else if((fillBigBox.classList.contains('collapsed') )&& (shareBigBox.classList.contains('collapsed'))){
+        designBigBox.classList.remove('collapsed');
+        designArrowDown.classList.remove('collapsed');
+        designArrowUp.classList.add('collapsed');
+        shareBigBox.classList.add('collapsed');
+        shareArrowDown.classList.remove('collapsed');
+        shareArrowUp.classList.add('collapsed');
+        fillBigBox.classList.add('collapsed');
+        fillArrowDown.classList.add('collapsed');
+        fillArrowUp.classList.remove('collapsed');
+    }
+    else{
+
+        shareBigBox.classList.add('collapsed');
+        shareArrowDown.classList.add('collapsed');
+        shareArrowUp.classList.remove('collapsed');
+    }
+  }
+
+ 
+
+fillHead.addEventListener('click', handleClickCollapsablesFill) ;
+shareHead.addEventListener('click', handleClickCollapsablesShare) ;
+designHead.addEventListener('click', handleClickCollapsablesDesign) ;
 
