bb5f00d6e1662e4972a5ba2e7b0ffbad48ac7a71
Ana Bollain <ana.bollain@gmail.com>
2022-11-04 17:55:20 +0100

    Optimized palette with for of

--- src/js/04_palette.js
+++ src/js/04_palette.js
@@ -1,60 +1,19 @@
 'use strict';
 
-/*//Create variable for article
-const previewArticleElement = document.querySelector('.js-article');
+//Array with all inputs
+const inputsPalette = document.querySelectorAll('.design__palette__box--selector');
 
-//Create variables for each palette radio input
-const paletteOne =  document.querySelector('.js-palette-one');
-const paletteTwo =  document.querySelector('.js-palette-two');
-const paletteThree =  document.querySelector('.js-palette-three');*/
 
-//Add even listener in paletteOne, add class and remove rest
-paletteOne.addEventListener('click',()=>{
-    previewArticleElement.classList.remove('paletteTwo'); 
-    previewArticleElement.classList.remove('paletteThree'); 
-    previewArticleElement.classList.add('paletteOne');    
-});
-
-//Add even listener in paletteTwo, add class and remove rest
-paletteTwo.addEventListener('click',()=>{
-    previewArticleElement.classList.remove('paletteOne'); 
-    previewArticleElement.classList.remove('paletteThree'); 
-    previewArticleElement.classList.add('paletteTwo');
-});
-
-//Add even listener in paletteThree, add class and remove rest
-paletteThree.addEventListener('click',()=>{
+//Function to add class depending on the checked input (currentTarget). Name value attribute for each input element in the HTML with the CSS class responding to each palette
+const checkPalette = (event)=>{
     previewArticleElement.classList.remove('paletteOne'); 
     previewArticleElement.classList.remove('paletteTwo'); 
-    previewArticleElement.classList.add('paletteThree');
-});
+    previewArticleElement.classList.remove('paletteThree');
+    previewArticleElement.classList.add(event.currentTarget.value);
+}
 
-/*ALTERNATIVE
-// Create variable for article and radio button
-const previewArticleElement = document.querySelector('.js-article');
-const radioInput =  document.querySelector('.design__palette');
+//For... of to add an event listener in each input
+for (input of inputsPalette){
+    input.addEventListener('click', checkPalette);
+}
 
-//Even listener for color palette changes in article
-
-radioInput.addEventListener('click',(event)=>{
-    console.log(event.target.tagName);
-    if(event.target.tagName === 'INPUT' && event.target.getAttribute('type') === 'radio' && event.target.value === '1' ){
-        console.log('paletteone');
-        previewArticleElement.classList.remove('paletteTwo'); 
-        previewArticleElement.classList.remove('paletteThree'); 
-        previewArticleElement.classList.add('paletteOne');    
-    }
-    if (event.target.tagName === 'INPUT' && event.target.getAttribute('type') === 'radio' && event.target.value === '2' ){
-        console.log('palettetwo');
-        previewArticleElement.classList.remove('paletteOne'); 
-        previewArticleElement.classList.remove('paletteThree'); 
-        previewArticleElement.classList.add('paletteTwo');
-    }
-    if(event.target.tagName === 'INPUT' && event.target.getAttribute('type') === 'radio' && event.target.value === '3' ){
-        console.log('palettethree');
-        previewArticleElement.classList.remove('paletteOne'); 
-        previewArticleElement.classList.remove('paletteTwo'); 
-        previewArticleElement.classList.add('paletteThree');
-    }
-});
-*/
