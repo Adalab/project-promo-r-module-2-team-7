a8e254891db4716188e4a85502fde8045d03aaa3
Ana Bollain <ana.bollain@gmail.com>
2022-11-07 17:57:23 +0100

    Update javascript

--- src/js/04_palette.js
+++ src/js/04_palette.js
@@ -1,17 +1,9 @@
 'use strict';
 
-/*//Create variable for article
-const previewArticleElement = document.querySelector('.js-article');
-
-//Create variables for each palette radio input
-const paletteOne =  document.querySelector('.js-palette-one');
-const paletteTwo =  document.querySelector('.js-palette-two');
-const paletteThree =  document.querySelector('.js-palette-three');*/
-
 function checkPalette(event){
+    previewArticleElement.classList.remove('paletteOne');
     previewArticleElement.classList.remove('paletteTwo'); 
     previewArticleElement.classList.remove('paletteThree'); 
-    previewArticleElement.classList.remove('paletteOne');
     previewArticleElement.classList.add(event.currentTarget.value);
 }
 
