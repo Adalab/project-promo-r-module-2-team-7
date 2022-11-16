44f681d7a5688dd8e7c2a8c79194c153ae9dfaf2
Marta Castrillo <mart.castrillo@gmail.com>
2022-11-16 13:48:55 +0100

    add form styles

--- src/js/09-validate-form.js
+++ src/js/09-validate-form.js
@@ -5,9 +5,9 @@ emailInput.addEventListener("input", (e) => {
     e.preventDefault();
     const emailInputValue = e.currentTarget.value;
     if( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(emailInputValue) !== true) {
-        emailInput.style.border = "thin solid red"
+        emailInput.style.border = "medium solid #d96863"
     } else {
-        emailInput.style.border = "thin solid #a2deaf";
+        emailInput.style.border = "medium solid #f8e055";
     }
 });
 
@@ -16,9 +16,9 @@ phoneInput.addEventListener("input", (e) => {
     e.preventDefault();
     const phoneInputValue = e.currentTarget.value;
     if( /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/.test(phoneInputValue) !== true) {
-        phoneInput.style.border = "thin solid red"
+        phoneInput.style.border = "medium solid #d96863"
     } else {
-        phoneInput.style.border = "thin solid #a2deaf";
+        phoneInput.style.border = "medium solid #f8e055";
     }
 });
 
@@ -27,9 +27,9 @@ linkedinInput.addEventListener("input", (e) => {
     e.preventDefault();
     const linkedinInputValue = e.currentTarget.value;
     if( /^[a-zA-Z0-9._-]{5,30}$/.test(linkedinInputValue) !== true) {
-        linkedinInput.style.border = "thin solid red"
+        linkedinInput.style.border = "medium solid #d96863"
     } else {
-        linkedinInput.style.border = "thin solid #a2deaf";
+        linkedinInput.style.border = "medium solid #f8e055";
     }
 });
 
@@ -38,8 +38,8 @@ githubInput.addEventListener("input", (e) => {
     e.preventDefault();
     const githubInputValue = e.currentTarget.value;
     if( /^[a-zA-Z0-9._-]{5,30}$/.test(githubInputValue) !== true) {
-        githubInput.style.border = "thin solid red"
+        githubInput.style.border = "medium solid #d96863"
     } else {
-        githubInput.style.border = "thin solid #a2deaf";
+        githubInput.style.border = "medium solid #f8e055";
     }
 });
